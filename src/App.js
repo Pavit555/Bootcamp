15d03634732cdebe2083e620ce68d3e31fafe9c5
Pavit Kaur <pavitsidhu123@gmail.com>
2023-07-03 21:21:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import UserForm from './UserForm';
+
+function App() {
+  return (
+    <div className="container">
+      <UserForm />
+    </div>
+  );
+}
+export default App;

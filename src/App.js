32729d7b1f07c380b17fc31bf6c9d9ac2e8225e6
Pavit555 <84263467+Pavit555@users.noreply.github.com>
2023-07-13 01:04:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import UserForm from './UserForm';
+import Navbar from './components';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import { Bars } from './components/NavbarElements';
 
 function App() {
   return (
     <div className="container">
+       <Router>
+            <Navbar />
+            <Bars />
+        </Router>
       <UserForm />
     </div>
   );

639a28c5b1463198786294a8808111bef6b7814d
Pavit555 <84263467+Pavit555@users.noreply.github.com>
2023-07-13 01:10:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import Navbar from './components';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import "./App.css";
+import { Bars } from './components/NavbarElements';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Search } from './search';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        Books with Hooks
-      </header>
+      <Router>
+         <Navbar />
+            <Bars />
+            </Router>
       <Search/>
     </div>
   );
